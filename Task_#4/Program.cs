15c9eb9e0d9edf5d977c33d02816ad82ec17c813
Aleksey Kushnerov <kuschneroff.alex@yandex.ru>
2022-10-17 23:02:42 +0300

    Решение задачи нахождения Факториала числа N

---
+++ Task_#4/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Решение задачи нахождения Факториала числа N
+*/
+double Factorial (int n) // также решается через int
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++) // при использовании int - программа начинает выдавать отрицательные числа, это не правильно. Зато double с этим справляется
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//Console.WriteLine(Factorial(5)); // 1*2*3 = 6
